8fa58f7677032844f0fffc9ac84c82acfb5e3cbe
Emily G <aspentree79@gmail.com>
2019-09-09 16:20:23 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -57,8 +57,8 @@ end
 
 def runner(card_total)
   welcome
-  initial_round
-  until display_card_total(card_total) > 21
+  card_total = initial_round
+  until card_total > 21
   hit?
   display_card_total
   end
